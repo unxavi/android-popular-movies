40702c0d3a41743947b6e85805ef82196363cfa4
Javier Vieira <xavi.vieira@gmail.com>
2018-02-23 10:55:35 +0100

    Refactor packages and layout for feed movies activity

--- app/src/main/java/com/icodehigh/popularmovies/features/moviesFeed/MoviesFeedActivity.java
+++ app/src/main/java/com/icodehigh/popularmovies/features/moviesFeed/MoviesFeedActivity.java
@@ -1,10 +1,12 @@
-package com.icodehigh.popularmovies;
+package com.icodehigh.popularmovies.features.moviesFeed;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.icodehigh.popularmovies.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
