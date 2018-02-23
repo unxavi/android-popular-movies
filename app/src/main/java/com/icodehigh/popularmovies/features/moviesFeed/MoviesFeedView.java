a852e63169dae6720eb231a322c841f15851e32c
Javier Vieira <xavi.vieira@gmail.com>
2018-02-23 13:19:27 +0100

    DOCS: Add java docs

--- app/src/main/java/com/icodehigh/popularmovies/features/moviesFeed/MoviesFeedView.java
+++ app/src/main/java/com/icodehigh/popularmovies/features/moviesFeed/MoviesFeedView.java
@@ -8,10 +8,30 @@ import java.util.List;
 
 public interface MoviesFeedView extends MvpView {
 
+    /**
+     * Method called if the Api Call fails for a device error
+     */
     void showInternetError();
 
+    /**
+     * Method called if the API call fails for a server error
+     */
+    void showServerError();
+
+    /**
+     * Show the loading view when we open the app
+     */
     void showLoading();
 
+    /**
+     * If the API call returns success but there are not movies this method is called
+     */
+    void showEmptyState();
+
+    /**
+     * Method call when the API rerutn sucess and there is a list of movies to show
+     * @param movies list to show on the activity
+     */
     void showMovieData(List<Movie> movies);
 
 
