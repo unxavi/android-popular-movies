956521ebd178aecf7de0229f22ded5f9cf86cb05
Javier Vieira <xavi.vieira@gmail.com>
2018-04-17 11:46:46 +0200

    make dbHelper package private

--- app/src/main/java/com/icodehigh/popularmovies/data/MovieDbHelper.java
+++ app/src/main/java/com/icodehigh/popularmovies/data/MovieDbHelper.java
@@ -20,7 +20,7 @@ public class MovieDbHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
 
-    public MovieDbHelper(Context context) {
+    MovieDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
